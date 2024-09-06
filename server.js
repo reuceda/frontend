2f11d362d3860a320f1e9138e9d923b44b24814d
astor.euceda <astor.euceda@gmail.com>
2024-09-05 22:20:36 -0600

    primer commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+
+// Configuración de EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Middleware para parsear el cuerpo de las solicitudes
+app.use(express.urlencoded({ extended: true }));
+
+// Configuración estática para Bootstrap
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Rutas
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/departamentos', (req, res) => {
+    res.render("departamentos");
+});
+
+app.listen(port, () => {
+    console.log(`Servidor corriendo en http://localhost:${port}`);
+});
